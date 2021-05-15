02864c19b0032ac52c7c03eeec26ef2d3f8043d4
Piotr Serafin <piotr.serafin.71@gmail.com>
2021-05-15 23:17:57 +0200

    Added lua functions to set options and keymaps.

---
+++ lua/utils/init.lua
@@ -0,0 +1,26 @@
+--   _____ _____
+--  |  _  |   __|  Piotr Serafin
+--  |   __|__   |  https://piotrserafin.dev
+--  |__|  |_____|  https://github.com/piotrserafin
+--
+
+local utils = {}
+
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+function utils.opt(scope, key, value)
+    scopes[key][value] = value
+    if scope ~= 'o' then
+        scopes['o'][key] = value
+    end
+end
+
+function utils.map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+return utils
