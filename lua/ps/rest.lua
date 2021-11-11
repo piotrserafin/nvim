0d8943d9393b01d8432fa3ade1a6f7ebaad49963
Piotr Serafin <piotr.serafin.71@gmail.com>
2021-11-11 10:28:34 +0100

    Change settings for rest.nvim

--- lua/ps/rest.lua
+++ lua/ps/rest.lua
@@ -6,10 +6,11 @@
 
 require('rest-nvim').setup({
     result_split_horizontal = false,
-    skip_ssl_verification = false
+    skip_ssl_verification = false,
+    jump_to_request = true,
 })
 
-vim.api.nvim_set_keymap('n', '<Leader>rr', '<Plug>RestNvim<CR>',        { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>rp', '<Plug>RestNvimPreview<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>rl', '<Plug>RestNvimLast<CR>',    { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>rr', '<Plug>RestNvim<CR>',        { noremap = false })
+vim.api.nvim_set_keymap('n', '<Leader>rp', '<Plug>RestNvimPreview<CR>', { noremap = false })
+vim.api.nvim_set_keymap('n', '<Leader>rl', '<Plug>RestNvimLast<CR>',    { noremap = false })
 
