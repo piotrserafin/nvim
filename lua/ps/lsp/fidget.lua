c9545a67c5528f6b71579f7997403e31252d7ed8
Piotr Serafin <piotr_serafin@epam.com>
2022-02-10 22:00:33 +0100

    Added fidget plugin. Removed luaref

---
+++ lua/ps/lsp/fidget.lua
@@ -0,0 +1,7 @@
+--   _____ _____
+--  |  _  |   __|  Piotr Serafin
+--  |   __|__   |  https://piotrserafin.dev
+--  |__|  |_____|  https://github.com/piotrserafin
+--
+
+require("fidget").setup({})
