bd6c2f77bc1a970be640d275a25555d85ab4bd32
Piotr Serafin <piotr_serafin@epam.com>
2022-02-10 22:56:37 +0100

    Migrated to luasnip

---
+++ lua/ps/luasnip.lua
@@ -0,0 +1,45 @@
+--   _____ _____
+--  |  _  |   __|  Piotr Serafin
+--  |   __|__   |  https://piotrserafin.dev
+--  |__|  |_____|  https://github.com/piotrserafin
+--
+
+local status_ok, ls = pcall(require, "luasnip")
+if not status_ok then
+    return
+end
+
+local types = require("luasnip.util.types")
+
+ls.config.set_config({
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { "<-", "Error" } },
+            },
+        },
+    },
+})
+
+local s = ls.s
+local t = ls.text_node
+
+ls.snippets = {
+    all = {
+        s("dupa", t("To jest dupa")),
+    },
+
+    lua = {
+        s("psh", {
+            t({
+                "--   _____ _____",
+                "--  |  _  |   __|  Piotr Serafin",
+                "--  |   __|__   |  https://piotrserafin.dev",
+                "--  |__|  |_____|  https://github.com/piotrserafin",
+                "--",
+            }),
+        }),
+    },
+}
