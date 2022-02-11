c5ad52a48258b38a112ab9fea49a97ef5ae258b1
Piotr Serafin <piotr.serafin.71@gmail.com>
2022-02-11 16:10:31 +0100

    Disable builtin plugins

--- lua/settings.lua
+++ lua/settings.lua
@@ -85,3 +85,40 @@ opt.clipboard:append("unnamedplus")
 opt.shortmess:append("c")
 
 opt.completeopt = "menu,menuone,noselect"
+
+-- Disable builtin vim plugins
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "matchit",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
+
+-- Disable Python2 support
+vim.g.loaded_python_provider = 0
+
+-- Disable perl provider
+vim.g.loaded_perl_provider = 0
+
+-- Disable ruby provider
+-- vim.g.loaded_ruby_provider = 0
+
+-- Disable node provider
+vim.g.loaded_node_provider = 0
