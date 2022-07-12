f0ed3352ffbf508654ce6b91493c773d16d9ad4c
Piotr Serafin <piotr.serafin.71@gmail.com>
2022-07-12 15:33:47 +0200

    Conditonaly use some plugins depending on OS

--- lua/ps/utils.lua
+++ lua/ps/utils.lua
@@ -4,14 +4,22 @@
 --  |__|  |_____|  https://github.com/piotrserafin
 --
 
-P = function(val)
+local M = {}
+
+function M.debug_print(val)
     print(vim.inspect(val))
     return val
 end
 
-DN = function(val, msg)
+function M.debug_notify(val, msg)
     local time = os.date("%H:%M")
     local message = (msg or " ") .. " " .. time
     require("notify")(vim.inspect(val), "debug", { title = { "Debug Output", message } })
     return val
 end
+
+function M.is_darwin()
+  return vim.loop.os_uname().sysname == 'Darwin'
+end
+
+return M
