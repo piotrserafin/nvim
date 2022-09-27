35fd754241fce3e09975b7226ffb70afb8dd8792
Piotr Serafin <piotr.serafin.71@gmail.com>
2022-09-27 11:41:23 +0200

    Fix wiki and calendar paths

--- lua/ps/vimwiki.lua
+++ lua/ps/vimwiki.lua
@@ -7,14 +7,14 @@
 -- VIMWIKI
 vim.g.vimwiki_list = {
     {
-        path = "~/Documents/vimwiki/personal.wiki/",
+        path = "~/Documents/vimwiki/personal.wiki",
         syntax = "markdown",
         ext = ".md",
         auto_diary_index = 1,
         auto_toc = 1,
     },
     {
-        path = "~/OneDrive_EPAM/vimwiki/work.wiki/",
+        path = "~/OneDrive - EPAM/vimwiki/work.wiki",
         syntax = "markdown",
         ext = ".md",
         auto_diary_index = 1,
@@ -28,7 +28,7 @@ vim.g.vimwiki_folding = "custom"
 vim.g.vimwiki_dir_link = "index"
 
 -- CALENDAR + VIMWIKI
-vim.g.calendar_diary = "~/OneDrive_EPAM/vimwiki/work.wiki/diary"
+vim.g.calendar_diary = "~/OneDrive - EPAM/vimwiki/work.wiki/diary"
 
 vim.cmd([[
 au BufNewFile ~/OneDrive_EPAM/vimwiki/work.wiki/diary/*.md :silent 0r !~/.config/nvim/scripts/generate-vimwiki-diary-template '%'
