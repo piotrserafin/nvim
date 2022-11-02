75d54de1ac4e163fbf1a795fddda533d14fd6c5a
Piotr Serafin <piotr.serafin.71@gmail.com>
2022-11-02 23:56:28 +0100

    Changed keymap for venn

--- lua/ps/venn.lua
+++ lua/ps/venn.lua
@@ -23,4 +23,4 @@ function _G.Toggle_venn()
     end
 end
 -- toggle keymappings for venn using <leader>v
-vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>vnn', ":lua Toggle_venn()<CR>", { noremap = true})
