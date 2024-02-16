61420d83df408da3eb96c7a842ffdb16f72715e8
Piotr Serafin <piotr.serafin.71@gmail.com>
2024-02-16 13:39:59 +0100

    Lazy load dap plugin

--- lua/ps/plugins/lsp/debug.lua
+++ lua/ps/plugins/lsp/debug.lua
@@ -7,6 +7,7 @@
 return {
     -- NOTE: Yes, you can install new plugins here!
     "mfussenegger/nvim-dap",
+    lazy = true,
     -- NOTE: And you can specify dependencies as well
     dependencies = {
         -- Creates a beautiful debugger UI
