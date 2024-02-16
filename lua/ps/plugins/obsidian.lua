7b04ac39e386a3969742e96229115a9b7fd4719f
Piotr Serafin <piotr.serafin.71@gmail.com>
2024-02-16 13:40:28 +0100

    More custom config for obsidian

--- lua/ps/plugins/obsidian.lua
+++ lua/ps/plugins/obsidian.lua
@@ -52,32 +52,58 @@ return {
             },
         },
 
+        notes_subdir = "00_Inbox",
+
+        daily_notes = {
+            subdir = "00_Inbox/daily",
+            date_format = "%Y-%m-%d",
+            template = nil,
+        },
+
         completion = {
             nvim_cmp = true,
             min_chars = 2,
+            new_notes_location = "notes_subdir",
         },
 
-        notes_subdir = "00_Inbox",
-
+        -- Example:
+        -- If title given: test note -> test-note
+        -- If title nil:   nil -> 163123123
         note_id_func = function(title)
-            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-            -- In this case a note with the title 'My new note' will be given an ID that looks
-            -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-            local suffix = ""
             if title ~= nil then
-                -- If title is given, transform it into valid file name.
-                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                title = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
             else
-                -- If title is nil, just add 4 random uppercase letters to the suffix.
-                for _ = 1, 4 do
-                    suffix = suffix .. string.char(math.random(65, 90))
+                title = tostring(os.time())
+            end
+            return title
+        end,
+
+        image_name_func = function()
+            -- Prefix image names with timestamp.
+            return string.format("%s-", os.time())
+        end,
+
+        note_frontmatter_func = function(note)
+            if note.title then
+                note:add_alias(note.title)
+            end
+
+            local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+            if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                for k, v in pairs(note.metadata) do
+                    out[k] = v
                 end
             end
-            return tostring(os.time()) .. "-" .. suffix
+
+            return out
         end,
 
         templates = {
             subdir = "99_Templates",
+            date_format = "%Y-%m-%d-%a",
+            time_format = "%H-%M",
+            tags = "",
         },
 
         open_app_foreground = true,
@@ -89,6 +115,11 @@ return {
 
         finder_mappings = {
             new = "<C-x>",
+            insert_link = "<C-l>",
+        },
+
+        attachments = {
+            img_folder = "97_Assets",
         },
     },
 }
