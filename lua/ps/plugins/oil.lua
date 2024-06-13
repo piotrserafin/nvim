0ede1f5c1f65b391abcf38e1c972a1d09c652d73
Piotr Serafin <piotr.serafin.71@gmail.com>
2024-06-13 08:00:47 +0200

    Add oil plugin

---
+++ lua/ps/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+    "stevearc/oil.nvim",
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+            natural_order = true,
+            is_always_hidden = function(name, _)
+                return name == ".." or name == ".git"
+            end,
+        },
+        win_options = {
+            wrap = true,
+        },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function(_, opts)
+        require("oil").setup(opts)
+        vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+    end,
+}
