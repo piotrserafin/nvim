f57ddfa4d410f37023821cfdbaffa1ce8f18af6a
Piotr Serafin <piotr.serafin.71@gmail.com>
2024-09-12 22:13:43 +0200

    Config for db plugin

--- lua/ps/plugins/db.lua
+++ lua/ps/plugins/db.lua
@@ -5,47 +5,23 @@
 --
 
 return {
-    {
-        "kristijanhusak/vim-dadbod-ui",
-        dependencies = {
-            { "tpope/vim-dadbod", lazy = true },
-            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-        },
-        cmd = {
-            "DBUI",
-            "DBUIToggle",
-            "DBUIAddConnection",
-            "DBUIFindBuffer",
-        },
-        keys = {
-            { "<Leader>qq", "<Cmd>DBUIToggle<CR>", desc = "DB Explorer Toggle" },
-        },
-        init = function()
-            vim.g.db_ui_show_help = 0
-            vim.g.db_ui_winwidth = 50
-            vim.g.db_ui_save_location = os.getenv("NVIM_DBUI_CONFIG")
-
-            local function db_completion()
-                require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-            end
-
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "sql",
-                },
-                command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-            })
-
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "sql",
-                    "mysql",
-                    "plsql",
-                },
-                callback = function()
-                    vim.schedule(db_completion)
-                end,
-            })
-        end,
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+        { "tpope/vim-dadbod", lazy = true },
+        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
+    cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
+    },
+    keys = {
+        { "<Leader>qq", "<Cmd>DBUIToggle<CR>", desc = "DB Explorer Toggle" },
+    },
+    config = function()
+        vim.g.db_ui_show_help = 0
+        vim.g.db_ui_winwidth = 50
+        vim.g.db_ui_save_location = os.getenv("NVIM_DBUI_CONFIG")
+    end,
 }
