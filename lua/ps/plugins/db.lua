28b25d9d3ae84bf5ef214e9c0a2e0e77121629a8
Piotr Serafin <piotr.serafin.71@gmail.com>
2024-12-30 13:57:03 +0100

    Disable vim-dadbod in favor of external tool - rainfrog

--- lua/ps/plugins/db.lua
+++ lua/ps/plugins/db.lua
@@ -6,6 +6,7 @@
 
 return {
     "kristijanhusak/vim-dadbod-ui",
+    enabled = false,
     dependencies = {
         { "tpope/vim-dadbod", lazy = true },
         { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
