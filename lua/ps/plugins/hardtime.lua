3e6594524edea880331786d63f7702628136b6a0
Piotr Serafin <piotr.serafin.71@gmail.com>
2025-05-29 17:40:02 +0300

    Added hardtime.nvim

---
+++ lua/ps/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    keys = {
+        { "<Leader>ht", "<Cmd>Hardtime toggle<CR>", desc = "Toggle hardtime" },
+    },
+    opts = {},
+}
